2bc3c8742b17045de66484aeb6b21f31f6af59b1
sagatake <takeshi-sa@live.jp>
2025-01-06 18:07:09 +0100

    Revert "add .gitignore"
    This reverts commit 0cb4a129d3485c38014d4dbc746633cce8d9144a.

--- tools/ThriftGreta/ThriftGreta-Make.bat
+++
@@ -1,5 +0,0 @@
-mkdir generated
-
-thrift-0.13.0 -o generated --gen java   ThriftGreta.thrift
-thrift-0.13.0 -o generated --gen csharp ThriftGreta.thrift
-thrift-0.13.0 -o generated --gen cpp    ThriftGreta.thrift
